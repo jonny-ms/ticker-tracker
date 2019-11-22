0e4fc840908bf709f169be397f9fde10f9b641c9
jonny-ms <j.milne21@gmail.com>
2019-11-22 13:00:18 -0500

    Add mock-api file so I dont use up all my free calls. Autocomplete simulates two objects from the array returned by a 'nbe' search term

---
+++ src/db/mock-api.js
@@ -0,0 +1,20 @@
+const autoComplete = [
+  {
+  symbol:"NBEV",
+  name:"New Age Beverages Corporation",
+  exch:"NMS",
+  type:"S",
+  exchDisp:"NASDAQ",
+  typeDisp:"Equity",
+  },
+  {
+  symbol:"NBEUROPA70FI.BC",
+  name:"NB Europa 70 FI",
+  exch:"BAR",
+  type:"M",
+  exchDisp:"Barcelona",
+  typeDisp:"Fund",
+  }
+]
+
+exports.autoComplete = autoComplete
